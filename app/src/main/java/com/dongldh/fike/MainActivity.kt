2b235ecb92b7a8831f39f16c76958c07e6551442
linear14 <47495592+linear14@users.noreply.github.com>
2020-07-27 17:45:13 +0900

    BottomSheet 세팅 및 디자인 - 폰트 설정 - FAB 보이기/감추기 설정 - 레이아웃 디자인 등

--- app/src/main/java/com/dongldh/fike/MainActivity.kt
+++ app/src/main/java/com/dongldh/fike/MainActivity.kt
@@ -3,16 +3,20 @@ package com.dongldh.fike
 import android.content.Context
 import android.graphics.Camera
 import android.os.AsyncTask
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
 import com.dongldh.fike.data.*
 import com.dongldh.fike.retrofit.RetrofitClient
 import com.dongldh.fike.util.DEFAULT_ZOOM_LEVEL
 import com.dongldh.fike.util.Hash
 import com.dongldh.fike.util.Permissions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 import net.daum.mf.map.api.CameraPosition
@@ -29,9 +33,9 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity(), MapView.MapViewEventListener, MapView.CurrentLocationEventListener {
     var myLocationMapPoint: MapPoint? = null
     lateinit var map: MapView
-    val stationsList = mutableSetOf<Station>()
     // var stationRepository: StationRepository? = null
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,63 +52,24 @@ class MainActivity : AppCompatActivity(), MapView.MapViewEventListener, MapView.
             map.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition(myLocationMapPoint, DEFAULT_ZOOM_LEVEL)))
         }
 
-        // 데이터가 Room에 잘 들어갔나 테스트할려고 작성한 코드입니다. (나중에 삭제 예정) 됐당!
-        searchFab.setOnClickListener {
-            AsyncTask.execute {
-                val db = AppDatabase.getInstance(applicationContext)
-                val list = db.stationDao().getStations()
-                for(i in 0..30) {
-                    Log.d("dataTest", "${list[i].stationId}, ${list[i].stationName}")
-                }
-            }
-        }
 
         map.setMapViewEventListener(this)
         map.setCurrentLocationEventListener(this)
 
-        getAllDatasFromRetrofit(1, 1000, 0)
-    }
+        RetrofitClient(applicationContext).getAllDatasFromRetrofit(1, 1000, 0)
 
-    fun getAllDatasFromRetrofit(start: Int, end: Int, total: Int) {
-        val retrofitClient = RetrofitClient()
-        val call = retrofitClient.apiService.getRetrofitData(start.toString(), end.toString())
-        call.enqueue(object: Callback<StationPOJO> {
-            override fun onFailure(call: Call<StationPOJO>, t: Throwable) {
-                Log.d("retrofitError", t.message!!)
-                Toast.makeText(this@MainActivity, "서버 접속 실패", Toast.LENGTH_SHORT).show()
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.halfExpandedRatio = 0.4f
+        bottomSheetBehavior.isFitToContents = false
+
+        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
             }
 
-            override fun onResponse(call: Call<StationPOJO>, response: Response<StationPOJO>) {
-                Toast.makeText(this@MainActivity, "서버 접속 성공", Toast.LENGTH_SHORT).show()
-                Log.d("retrofit", response?.body().toString())
-                if(response.isSuccessful) {
-                    Log.d("retrofitData", "데이터 받아오기 성공")
-
-                    val rentBikeStatus = response.body()!!.rentBikeStatus
-                    val listTotalCount = rentBikeStatus.listTotalCount
-                    val dataArray = rentBikeStatus.row
-
-                    for (data in dataArray) {
-                        // 룸 데이터베이스에 값 모두 집어넣기
-                        stationsList.add(Station(data.stationId, data.stationName, data.parkingBikeToCnt, data.latitude, data.longitude))
-                        /*val stationId = data.stationId
-                        Log.d("stationId", stationId)*/
-                    }
-                    if(end <= total + listTotalCount) {
-                        getAllDatasFromRetrofit(start+1000, end+1000, total+1000)
-                    } else {
-                        // 나중에 만들 progress bar 멈추도록 설정 + RoomDatabase에 insert (이런거 나중에 다 MVVM으로 바꾸자)
-
-                        // deprecated 될 예정. 더 좋은 방법은?
-                        AsyncTask.execute {
-                            val db = AppDatabase.getInstance(applicationContext)
-                            db.stationDao().deleteAll()
-                            db.stationDao().insertStations(stationsList)
-                        }
-                    }
-
-                } else {
-                    Log.d("retrofitData", "데이터 받아오기 실패")
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> myLocationFab.hide()
+                    else -> myLocationFab.show()
                 }
             }
 
--- app/src/main/java/com/dongldh/fike/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/dongldh/fike/retrofit/RetrofitClient.kt
@@ -1,22 +1,23 @@
 package com.dongldh.fike.retrofit
 
+import android.content.Context
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.Toast
 import com.dongldh.fike.BuildConfig
+import com.dongldh.fike.data.AppDatabase
+import com.dongldh.fike.data.Station
+import com.dongldh.fike.data.StationPOJO
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitClient {
-    private fun okHttpClientBuilder(): OkHttpClient.Builder {
-        val okhttpClientBuilder = OkHttpClient.Builder()
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-        okhttpClientBuilder.addInterceptor(logging)
-
-        return okhttpClientBuilder
-    }
-
+class RetrofitClient(val context: Context) {
     val gson = GsonBuilder()
         .setLenient()
         .create()
@@ -29,5 +30,62 @@ class RetrofitClient {
         .build()
 
     val apiService: ApiService = retrofit.create(ApiService::class.java)
+
+    // For Logging
+    private fun okHttpClientBuilder(): OkHttpClient.Builder {
+        val okhttpClientBuilder = OkHttpClient.Builder()
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        okhttpClientBuilder.addInterceptor(logging)
+
+        return okhttpClientBuilder
+    }
+
+    fun getAllDatasFromRetrofit(start: Int, end: Int, total: Int) {
+        val stationsList = mutableSetOf<Station>()
+        val call = this.apiService.getRetrofitData(start.toString(), end.toString())
+
+        call.enqueue(object: Callback<StationPOJO> {
+            override fun onFailure(call: Call<StationPOJO>, t: Throwable) {
+                Log.d("retrofitError", t.message!!)
+                Toast.makeText(context, "서버 접속 실패", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<StationPOJO>, response: Response<StationPOJO>) {
+                Toast.makeText(context, "서버 접속 성공", Toast.LENGTH_SHORT).show()
+                Log.d("retrofit", response?.body().toString())
+                if(response.isSuccessful) {
+                    Log.d("retrofitData", "데이터 받아오기 성공")
+
+                    val rentBikeStatus = response.body()!!.rentBikeStatus
+                    val listTotalCount = rentBikeStatus.listTotalCount
+                    val dataArray = rentBikeStatus.row
+
+                    for (data in dataArray) {
+                        // 룸 데이터베이스에 값 모두 집어넣기
+                        stationsList.add(Station(data.stationId, data.stationName, data.parkingBikeToCnt, data.latitude, data.longitude))
+                        /*val stationId = data.stationId
+                        Log.d("stationId", stationId)*/
+                    }
+                    if(end <= total + listTotalCount) {
+                        getAllDatasFromRetrofit(start+1000, end+1000, total+1000)
+                    } else {
+                        // 나중에 만들 progress bar 멈추도록 설정 + RoomDatabase에 insert (이런거 나중에 다 MVVM으로 바꾸자)
+
+                        // deprecated 될 예정. 더 좋은 방법은?
+                        AsyncTask.execute {
+                            val db = AppDatabase.getInstance(context.applicationContext)
+                            db.stationDao().deleteAll()
+                            db.stationDao().insertStations(stationsList)
+                        }
+                    }
+
+                } else {
+                    Log.d("retrofitData", "데이터 받아오기 실패")
+                }
+            }
+
+        })
+    }
 
 }
